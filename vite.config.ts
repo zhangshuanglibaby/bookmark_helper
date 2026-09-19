1fb3be4879b372152bd20cac22d373c142ccc8e8
zhangshuanglibaby <943165920@qq.com>
2026-09-19 21:47:24 +0800

    修改 Vite 配置，让它读取 manifest.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+// 从 Vite 引入定义配置的工具。
+import { defineConfig } from "vite";
+// 引入 React 的 Vite 插件，让 Vite 能处理 React 组件。
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// 引入 CRXJS 插件，让 Vite 能打包 Chrome 扩展。
+import { crx } from "@crxjs/vite-plugin";
+
+// 引入Chrome 扩展配置。
+import manifest from "./manifest.config.ts";
+
+
+// 导出 Vite 的完整配置。
 export default defineConfig({
-  plugins: [react()],
+  // 配置项目需要使用的插件。
+  plugins: [
+    // 启用 React 支持。
+    react(),
+
+    // 启用 Chrome 扩展支持，并传入扩展信息配置。
+    crx({ manifest }),
+  ]
 })
